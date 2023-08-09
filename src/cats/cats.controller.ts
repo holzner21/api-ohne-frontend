26a30c1b7a4ea03dc9a5bad8e9c7e41a90a55172
Noah Tremmel <n.tremmel@one-unity.de>
2023-08-09 19:17:24 +0200

    created cat service

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Post, Param, Body, Put, Delete, Query } from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { ListAllEntities } from './dto/list-all-entities.dto';
+import { UpdateCatDto } from './dto/update-cat.dto';
+import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cat.interface';
+
+@Controller('cats')
+export class CatsController {
+    constructor(private catsService: CatsService) {}
+
+    @Post()
+    async create(@Body() createCatDto: CreateCatDto) {
+        this.catsService.create(createCatDto);
+    }
+
+    @Get()
+    async findAll(): Promise<Cat[]> {
+        return this.catsService.findAll();
+    }
+}
